5c951a93fef90efb97be283f7bb60813c624d7e0
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2024-02-26 16:16:49 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CV Application
+
+An application to create your curriculum vitae. Made using React and Vite.
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./styles/App.css";
+
+function App() {
+  return (
+    <>
+      <div>New project!</div>
+      <h1>This is heading 1</h1>
+      <a href="#">This is an anchor</a>
+      <button>This is a button.</button>
+    </>
+  );
+}
+
+export default App;
