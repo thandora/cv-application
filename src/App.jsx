9753bb153d8c318e6dcc4136e92e22978461ab93
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2024-03-03 14:36:23 +0800

    remote test components

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import "./styles/App.css";
 import { Form } from "./components/Form";
 import { sampleFields, sampleForm, sampleFormList } from "./components/sampleData";
 import { FormList } from "./components/FormList";
+import { useState } from "react";
 
 function App() {
   return (
     <>
-      <div>New project!</div>
-      <h1>This is heading 1</h1>
-      <a href="#">This is an anchor</a>
-      <button>This is a button.</button>
-      <Form form={sampleForm} isDeletable={false} />
+      <Form form={sampleForm} isDeletable={true} />
       <FormList formList={sampleFormList} />
     </>
   );
