dd9790b876155df3165ae8eacd5a3b7e8f1bffcc
Christian Klint Labadia <51294367+thandora@users.noreply.github.com>
2024-03-04 17:25:52 +0800

    add other form data and unique ID for each

--- src/components/sampleData.js
+++ src/components/sampleData.js
@@ -1,23 +1,61 @@
-const sampleFields = [
-  { id: 0, name: "First field", type: "text", value: "" },
-  { id: 1, name: "Another field", type: "text", value: "" },
-  { id: 2, name: "Age", type: "number", value: 33 },
+const fieldsPersonal = [
+  { id: crypto.randomUUID(), name: "Full name", type: "text", value: "John A. Doe" },
+  { id: crypto.randomUUID(), name: "Email", type: "email", value: "jdoe@email.email" },
+  { id: crypto.randomUUID(), name: "Contact Number", type: "text", value: "10987654321" },
+  { id: crypto.randomUUID(), name: "Address", type: "text", value: "Some Street, City, State" },
+  { id: crypto.randomUUID(), name: "Personal portfolio", type: "text", value: "jdoe.com" },
 ];
 
-const sampleFields1 = [
-  { id: 0, name: "Title", type: "text", value: "" },
-  { id: 1, name: "Location", type: "text", value: "" },
-  { id: 2, name: "years", type: "number", value: 33 },
+const fieldsEducation = [
+  { id: crypto.randomUUID(), name: "University Name", type: "text", value: "Some University" },
+  { id: crypto.randomUUID(), name: "Degree", type: "text", value: "Computer Science" },
+  { id: crypto.randomUUID(), name: "Graduation Month", type: "text", value: "March" },
+  { id: crypto.randomUUID(), name: "Graduation Year", type: "text", value: "2024" },
+  { id: crypto.randomUUID(), name: "Address", type: "text", value: "Some City, State" },
+  { id: crypto.randomUUID(), name: "Highlights", type: "text", value: "jdoe.com" },
 ];
 
-const sampleFields2 = [
-  { id: 0, name: "Title", type: "text", value: "" },
-  { id: 1, name: "Location", type: "text", value: "" },
-  { id: 2, name: "years", type: "number", value: 33 },
+const fieldsWork = [
+  { id: crypto.randomUUID(), name: "Company Name", type: "text", value: "Krusty Krab" },
+  { id: crypto.randomUUID(), name: "Job Title", type: "text", value: "Fry Cook" },
+  { id: crypto.randomUUID(), name: "Month start", type: "text", value: "May" },
+  { id: crypto.randomUUID(), name: "Year start", type: "text", value: "1999" },
+  { id: crypto.randomUUID(), name: "Month end", type: "text", value: "March" },
+  { id: crypto.randomUUID(), name: "Year end", type: "text", value: "2024" },
+  { id: crypto.randomUUID(), name: "Address", type: "text", value: "Some City, State" },
+  { id: crypto.randomUUID(), name: "Responsibility", type: "text", value: "jdoe.com" },
 ];
 
-const sampleForm = { id: 0, title: "MoonBucks", fields: sampleFields1 };
-const sampleForm1 = { id: 1, title: "DcMonalds", fields: sampleFields2 };
+const sampleFormPersonal = { id: crypto.randomUUID(), title: "Personal", fields: fieldsPersonal };
 
-const sampleFormList = { title: "Personal Details", forms: [sampleForm, sampleForm1] };
-export { sampleFields, sampleForm, sampleFormList };
+const sampleFormEducation = {
+  id: crypto.randomUUID(),
+  title: "Education",
+  fields: fieldsEducation,
+};
+
+const sampleFormWork = {
+  id: crypto.randomUUID(),
+  title: "Work Experience",
+  fields: fieldsWork,
+};
+
+const sampleFormListPersonal = {
+  id: crypto.randomUUID(),
+  title: "Personal",
+  forms: [sampleFormPersonal],
+};
+
+const sampleFormListEducation = {
+  id: crypto.randomUUID(),
+  title: "Education",
+  forms: [sampleFormEducation],
+};
+
+const sampleFormListWork = {
+  id: crypto.randomUUID(),
+  title: "Work Experienceaaaa",
+  forms: [sampleFormWork],
+};
+
+export { sampleFormListPersonal, sampleFormListEducation, sampleFormListWork };
